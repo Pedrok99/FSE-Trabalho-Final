b70733b64386f6900a8cfb367e7cd9f5ac365377
Pedrok99 <pedrohlima20@gmail.com>
2022-04-30 19:20:07 -0300

    create status component

---
+++ central/src/components/Status/index.tsx
@@ -0,0 +1,31 @@
+import { Center, Circle, Text } from '@chakra-ui/react'
+
+import './styles.css'
+import { IEspStatus } from '../../types/ESPTypes'
+
+function Status({ status }: IEspStatus) {
+  const statusColor = {
+    Conectado: {
+      text: 'green.700',
+      circle: 'green'
+    },
+    Desconectado: {
+      text: 'red.700',
+      circle: 'red'
+    }
+  }
+  return (
+    <Center gap={2}>
+      <Circle
+        size={3}
+        bg={`${statusColor[status].circle}`}
+        className={`${status === 'Conectado' ? 'pulsate' : ''}`}
+      />
+      <Text fontSize="sm" color={`${statusColor[status].text}`}>
+        {status}
+      </Text>
+    </Center>
+  )
+}
+
+export default Status
