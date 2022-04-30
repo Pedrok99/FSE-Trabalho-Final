6a803cb14c5d0d48f63334338f253fae4ba2937e
Pedrok99 <pedrohlima20@gmail.com>
2022-04-30 19:37:49 -0300

    mount basic layout
    Co-authored-by: Matheus73 <matheuscoding@gmail.com>

--- central/src/App.tsx
+++ central/src/App.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { Grid, GridItem, useDisclosure } from '@chakra-ui/react'
+import { useState } from 'react'
+
+import AddForm from './components/Form'
+import Loading from './components/Loading'
+import AddESPModal from './components/Modal'
+import MonitoringCard from './components/MonitoringCard'
+import Navbar from './components/Navbar'
+import { IEspInfo } from './types/ESPTypes'
 
 function App() {
-  return <h1>Alow</h1>
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [ESPsInfo, setESPsInfo] = useState<IEspInfo[]>([])
+
+  const addEspInfo = (espInfo: IEspInfo) => {
+    setESPsInfo([...ESPsInfo, espInfo])
+  }
+
+  return (
+    <>
+      <Navbar openAddModal={onOpen} />
+      {ESPsInfo.length > 0 ? (
+        <Grid templateColumns="repeat(5, 1fr)" gap={6} p="10">
+          {ESPsInfo.map((espInfo: IEspInfo) => (
+            <GridItem key={espInfo.espId}>
+              <MonitoringCard
+                espStatus={{ status: 'Desconectado' }}
+                espInfo={espInfo}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      ) : (
+        <Loading barSize="sm" title="Aguardando ESP32" />
+      )}
+      <AddESPModal title="Adicionar ESP" isOpen={isOpen} onClose={onClose}>
+        <AddForm addEspInfo={addEspInfo} onClose={onClose} />
+      </AddESPModal>
+    </>
+  )
 }
 
 export default App
