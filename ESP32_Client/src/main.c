e91a311c0e9f42e45a8c089ce058feadfcfdaa83
matheus73 <matheuscoding@gmail.com>
2022-05-01 12:21:19 -0300

    Add new mqtt library
    Co-authored-by: Pedrok99 <pedrohlima20@gmail.com>

--- ESP32_Client/src/main.c
+++ ESP32_Client/src/main.c
@@ -2,27 +2,55 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "nvs_flash.h"
+#include "freertos/semphr.h"
+
 #include "wifi.h"
+#include "mqtt.h"
 
-void app_main(){
+xSemaphoreHandle conexaoMQTTSemaphore;
 
-    //init NVS
-    esp_err_t ret = nvs_flash_init()
-    ;
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+void trataComunicacaoComServidor(void *params)
+{
+    char mensagem[50];
+    if (xSemaphoreTake(conexaoMQTTSemaphore, portMAX_DELAY))
+    {
+        while (true)
+        {
+            float temperatura = 20.0 + (float)rand() / (float)(RAND_MAX / 10.0);
+            sprintf(mensagem, "temperatura1: %f", temperatura);
+            mqtt_envia_mensagem("sensores/temperatura", mensagem);
+            vTaskDelay(3000 / portTICK_PERIOD_MS);
+        }
+    }
+}
+
+void app_main()
+{
+
+    // init NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
-    ESP_ERROR_CHECK( ret );
+    ESP_ERROR_CHECK(ret);
 
     wifi_start();
 
+    conexaoMQTTSemaphore = xSemaphoreCreateBinary();
+
+    mqtt_start();
+
     DHT11_init(GPIO_NUM_4);
 
-    while(1){
-        printf("Temperature is %d \n", DHT11_read().temperature);
-        printf("Humidity is %d\n", DHT11_read().humidity);
-        printf("Status code is %d\n", DHT11_read().status);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-    }
+    xTaskCreate(&trataComunicacaoComServidor, "Comunicação com Broker", 4096, NULL, 1, NULL);
+
+    // while (1)
+    // {
+    //     printf("Temperature is %d \n", DHT11_read().temperature);
+    //     printf("Humidity is %d\n", DHT11_read().humidity);
+    //     printf("Status code is %d\n", DHT11_read().status);
+    //     vTaskDelay(2000 / portTICK_PERIOD_MS);
+    // }
 }
