d02928245eaa7e523955b4231920b7d65569d861
matheus73 <matheuscoding@gmail.com>
2022-05-03 13:37:46 -0300

    :sparkles: Send data from sensor to broker
    Co-authored-by: Pedrok99 <pedrohlima20@gmail.com>

--- ESP32_Client/src/main.c
+++ ESP32_Client/src/main.c
@@ -8,48 +8,85 @@
 #include "esp_system.h"
 #include "mqtt.h"
 
+#include <pthread.h>
 
 #define TAG "Main"
 
 xSemaphoreHandle wifi_semaphore;
+xSemaphoreHandle conexaoMQTTSemaphore;
 
-void onWifiConnected(void *params){
+void get_sensor_data(void *pvParameters)
+{
+    DHT11_init(GPIO_NUM_4);
+    int initial_temperature = DHT11_read().temperature;
+    int initial_humidity = DHT11_read().humidity;
+
+    float temperature_avg = (float)initial_temperature;
+    float humidity_avg = (float)initial_humidity;
+
+    char tmp_data[50];
+    while (1)
+    {
+        if (xSemaphoreTake(conexaoMQTTSemaphore, portMAX_DELAY))
+        {
+            struct dht11_reading data = DHT11_read();
+            if (data.status == -1)
+            {
+                ESP_LOGE(TAG, "Error reading sensor");
+            }
+            else
+            {
+                temperature_avg = (temperature_avg * 0.8) + (float)data.temperature * 0.2;
+                humidity_avg = (humidity_avg * 0.8) + (float)data.humidity * 0.2;
+            }
+            sprintf(tmp_data, "{\"data\": %f}", temperature_avg);
+            ESP_LOGI(TAG, "Temperatura: %f", temperature_avg);
+            mqtt_envia_mensagem("/fse2021/180106970/sala/temperatura", tmp_data);
+            vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+            sprintf(tmp_data, "{\"data\": %f}", humidity_avg);
+            ESP_LOGI(TAG, "Umidade: %f", humidity_avg);
+            mqtt_envia_mensagem("/fse2021/180106970/sala/umidade", tmp_data);
+
+            vTaskDelay(2000 / portTICK_PERIOD_MS);
+            xSemaphoreGive(conexaoMQTTSemaphore);
+        }
+    }
+}
+
+void onWifiConnected(void *params)
+{
     while (true)
     {
-        if(xSemaphoreTake(wifi_semaphore, portMAX_DELAY)){
+        if (xSemaphoreTake(wifi_semaphore, portMAX_DELAY))
+        {
             ESP_LOGI(TAG, "Conectado ao wifi");
+
             mqtt_start();
 
+            ESP_LOGI(TAG, "Conecção com o broker mqtt feita");
         }
+
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
 
-void app_main(){
+void app_main()
+{
 
-    //init NVS
-    esp_err_t ret = nvs_flash_init()
-    ;
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    // init NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
-    ESP_ERROR_CHECK( ret );
-
+    ESP_ERROR_CHECK(ret);
+    conexaoMQTTSemaphore = xSemaphoreCreateBinary();
     wifi_semaphore = xSemaphoreCreateBinary();
 
     wifi_start();
 
     xTaskCreate(&onWifiConnected, "onWifiConnected", 2048, NULL, 1, NULL);
-
-
-    // DHT11_init(GPIO_NUM_4);
-
-    // while(1){
-    //     printf("Temperature is %d \n", DHT11_read().temperature);
-    //     printf("Humidity is %d\n", DHT11_read().humidity);
-    //     printf("Status code is %d\n", DHT11_read().status);
-    //     vTaskDelay(2000 / portTICK_PERIOD_MS);
-    //     mqtt_start();
-    // }
+    xTaskCreate(&get_sensor_data, "get_sensor_data", 2048, NULL, 1, NULL);
 }
--- ESP32_Client/src/mqtt.c
+++ ESP32_Client/src/mqtt.c
@@ -21,7 +21,7 @@
 
 #define TAG "MQTT"
 
-
+extern xSemaphoreHandle conexaoMQTTSemaphore;
 esp_mqtt_client_handle_t client;
 
 static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
@@ -29,7 +29,7 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
     esp_mqtt_client_handle_t client = event->client;
     int msg_id;
 
-    //send initial data
+    // send initial data
 
     // get mac
     uint8_t mac[6];
@@ -40,58 +40,61 @@ static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
     sprintf(mac_str, "%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     ESP_LOGI(TAG, "MAC: %s", mac_str);
 
-    switch (event->event_id) {
-        case MQTT_EVENT_CONNECTED:
-            ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+    switch (event->event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        // xSemaphoreTake(conexaoMQTTSemaphore, portMAX_DELAY);
 
-            char config_topic[255];
-            // fse2021/<matricula>/dispositivos/<ID_do_dispositivo>
-            sprintf(config_topic, "/%s/%s/dispositivos/%s", CONFIG_ESP_ROOT_TOPIC, CONFIG_ESP_MATRICULA, mac_str);
-               
-            msg_id = esp_mqtt_client_publish(client, config_topic, "{key:value}", 0, 1, 0);
-            ESP_LOGI(TAG, "sent publish to %s, msg_id=%d", config_topic, msg_id);
+        char config_topic[255];
+        // fse2021/<matricula>/dispositivos/<ID_do_dispositivo>
+        sprintf(config_topic, "/%s/%s/dispositivos/%s", CONFIG_ESP_ROOT_TOPIC, CONFIG_ESP_MATRICULA, mac_str);
 
+        msg_id = esp_mqtt_client_publish(client, config_topic, "{key:value}", 0, 1, 0);
+        ESP_LOGI(TAG, "sent publish to %s, msg_id=%d", config_topic, msg_id);
 
-            msg_id = esp_mqtt_client_subscribe(client, config_topic, 0);
-            ESP_LOGI(TAG, "Subscribed to %s, msg_id=%d", config_topic, msg_id);
+        msg_id = esp_mqtt_client_subscribe(client, config_topic, 0);
+        ESP_LOGI(TAG, "Subscribed to %s, msg_id=%d", config_topic, msg_id);
 
-            break;
+        xSemaphoreGive(conexaoMQTTSemaphore);
 
-        case MQTT_EVENT_DISCONNECTED:
-            ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-            break;
+        break;
 
-        case MQTT_EVENT_SUBSCRIBED:
-            ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-            msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-            ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-            break;
-        case MQTT_EVENT_UNSUBSCRIBED:
-            ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-            break;
-        case MQTT_EVENT_PUBLISHED:
-            ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-            break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
 
-        case MQTT_EVENT_DATA:
-            ESP_LOGI(TAG, "MQTT_EVENT_DATA_TRIGGERED");
-            // handleTopicEvent(event);
-            if(strcmp(event->topic, config_topic)==0){
-                printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-                printf("DATA=%.*s\r\n", event->data_len, event->data);
-            }
-            break;
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
 
-        case MQTT_EVENT_ERROR:
-            ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-            break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA_TRIGGERED");
+        // handleTopicEvent(event);
+        if (strcmp(event->topic, config_topic) == 0)
+        {
+            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+            printf("DATA=%.*s\r\n", event->data_len, event->data);
+        }
+        break;
 
-        default:
-            ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-            break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        break;
 
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
     }
-  return ESP_OK;
+    return ESP_OK;
 }
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
@@ -100,21 +103,21 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     mqtt_event_handler_cb(event_data);
 }
 
-void mqtt_start(){
- 
-  esp_mqtt_client_config_t mqtt_config = {
-    // .uri = "mqtt://mqtt.eclipse.org",
-    .uri = "mqtt://broker.hivemq.com",
-  };
+void mqtt_start()
+{
 
-//   esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_config);
-  client = esp_mqtt_client_init(&mqtt_config);
-  esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
-  esp_mqtt_client_start(client);
- 
+    esp_mqtt_client_config_t mqtt_config = {
+        // .uri = "mqtt://mqtt.eclipse.org",
+        .uri = "mqtt://broker.hivemq.com",
+    };
+
+    //   esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_config);
+    client = esp_mqtt_client_init(&mqtt_config);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
+    esp_mqtt_client_start(client);
 }
 
-void mqtt_envia_mensagem(char * topico, char * mensagem)
+void mqtt_envia_mensagem(char *topico, char *mensagem)
 {
     int message_id = esp_mqtt_client_publish(client, topico, mensagem, 0, 1, 0);
     ESP_LOGI(TAG, "Mesnagem enviada, ID: %d", message_id);
