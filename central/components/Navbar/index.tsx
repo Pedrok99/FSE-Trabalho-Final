7e930d12baa94124cfd41369d95e6ccc9de5bc4e
Pedrok99 <pedrohlima20@gmail.com>
2022-05-04 16:55:17 -0300

    update form fields and navbar
    Co-authored-by: Matheus73 <matheuscoding@gmail.com>

--- central/components/Navbar/index.tsx
+++ central/components/Navbar/index.tsx
@@ -1,30 +1,35 @@
-import { Button, Center, Flex, Spacer, Text } from '@chakra-ui/react'
-import { BiMicrochip } from 'react-icons/bi'
+import {
+ Button, Center, Divider, Flex, Spacer, Text 
+} from '@chakra-ui/react';
+import { BiMicrochip } from 'react-icons/bi';
 
 interface INavbarProps {
-  openAddModal: () => void
+  openAddModal: () => void;
 }
 
 function Navbar({ openAddModal }: INavbarProps) {
   return (
-    <Flex bgGradient="linear-gradient(-3deg, rgba(2,0,36,1) 0%, rgba(75,75,149,0.9920343137254902) 46%, rgba(0,212,255,1) 100%)">
-      <Center w="10%" h="16">
-        <BiMicrochip size={30} />
-        <Text ml={2}>FSE</Text>
-      </Center>
-      <Spacer />
-      <Center w="10%">
-        <Button
-          colorScheme="telegram"
-          size="sm"
-          fontSize="sm"
-          onClick={openAddModal}
-        >
-          Adicionar ESP32
-        </Button>
-      </Center>
-    </Flex>
-  )
+    <>
+      <Flex>
+        <Center w="10%" h="16">
+          <BiMicrochip size={30} />
+          <Text ml={2}>FSE</Text>
+        </Center>
+        <Spacer />
+        <Center w="10%">
+          <Button
+            colorScheme="telegram"
+            size="sm"
+            fontSize="sm"
+            onClick={openAddModal}
+          >
+            Adicionar ESP32
+          </Button>
+        </Center>
+      </Flex>
+      <Divider />
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
