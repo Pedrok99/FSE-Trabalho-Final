8163cdb7fcdc52ada05b46bd9fc5ac80d27ab29e
Pedrok99 <pedrohlima20@gmail.com>
2022-05-04 16:54:13 -0300

    add toastfy
    Co-authored-by: Matheus73 <matheuscoding@gmail.com>

--- central/pages/_app.tsx
+++ central/pages/_app.tsx
@@ -1,13 +1,24 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ToastContainer } from 'react-toastify';
 import { MqttProvider } from '../contexts/MqttContext';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <MqttProvider>
         <Component {...pageProps} />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          draggable={false}
+          closeOnClick
+          pauseOnHover
+        />
       </MqttProvider>
     </ChakraProvider>
   );
